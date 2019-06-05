794d3bc3e861242076c0b2c4f8b7c95dc961b1e7
Sumit Saini <sumit.saini@nagarro.com>
2019-06-05 10:16:18 +0530

    routes changed

--- biddermanagement/Controllers/ValuesController.cs
+++ biddermanagement/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace biddermanagement.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("bidders/api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
@@ -14,7 +14,7 @@ namespace biddermanagement.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "inside", "bidder" };
         }
 
         // GET api/values/5
